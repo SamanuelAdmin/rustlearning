763ae188b36bdbc327d96ac87f9dd2d8b5fb7eb5
SamanuelAdmin <samolenkosvatik93@gmail.com>
2026-01-01 22:38:47 +0200

    04/4-09/1

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -1,3 +1,6 @@
+use std::cell::Cell;
+
+
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -6,18 +9,18 @@ struct Order {
     made_by_mobile: bool,
     made_by_email: bool,
     item_number: u32,
-    count: u32,
+    count: Cell<u32>,
 }
 
-fn create_order_template() -> Order {
+fn create_order_template(new_name: String) -> Order {
     Order {
-        name: String::from("Bob"),
+        name: new_name,
         year: 2019,
         made_by_phone: false,
         made_by_mobile: false,
         made_by_email: true,
         item_number: 123,
-        count: 0,
+        count: 0.into(),
     }
 }
 
@@ -31,10 +34,20 @@ mod tests {
 
     #[test]
     fn your_order() {
-        let order_template = create_order_template();
+        let order_template = create_order_template(String::from("Bob"));
 
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+//        let your_order = Order {
+//            name: String::from("Hacker in Rust"),
+//            year: 2019,
+//            made_by_phone: false,
+//            made_by_mobile: false,
+//            made_by_email: true,
+//            item_number: 123,
+//            count: 1
+//        };
+        let your_order = create_order_template(String::from("Hacker in Rust"));
+        your_order.count.set(1);
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
@@ -42,6 +55,6 @@ mod tests {
         assert_eq!(your_order.made_by_mobile, order_template.made_by_mobile);
         assert_eq!(your_order.made_by_email, order_template.made_by_email);
         assert_eq!(your_order.item_number, order_template.item_number);
-        assert_eq!(your_order.count, 1);
+        assert_eq!(your_order.count, 1.into());
     }
 }
